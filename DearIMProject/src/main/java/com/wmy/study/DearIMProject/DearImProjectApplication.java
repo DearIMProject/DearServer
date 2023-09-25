aefbde9f9d497179e633aa08d2905ccd5562518a
APPLE <wenmingyan1990@gmail.com>
2023-09-25 10:02:22 +0800

    [feature] springboot init

---
+++ DearIMProject/src/main/java/com/wmy/study/DearIMProject/DearImProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wmy.study.DearIMProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearImProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearImProjectApplication.class, args);
+	}
+
+}
---
+++ DearIMProject/src/test/java/com/wmy/study/DearIMProject/DearImProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmy.study.DearIMProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearImProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
