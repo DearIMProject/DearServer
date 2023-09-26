cd3db9b92957fb21632b9d489ef9b30cbadb4c1d
APPLE <wenmingyan1990@163.com>
2023-09-26 14:41:58 +0800

    start aliyun initial

--- DearIMProject/DearIMProject-start/src/test/java/com/wmy/study/DearIMProject/DearImProjectApplicationTests.java
+++ DearIMProject/DearIMProject-start/src/test/java/com/wmy/study/DearIMProject/DearImProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DearImProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
